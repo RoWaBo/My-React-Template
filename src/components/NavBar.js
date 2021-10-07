d171e3db5686a69b6ba7e7807bca8e78cbb5306d
Robert <u1501851@rts-365.dk>
2021-10-07 14:09:48 +0200

    Added dark layer to view when mobile menu is open

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,10 +13,14 @@ const NavBar = ({ shadow }) => {
     const { colors, font, spacing, breakPoints } = useTheme()
     const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-    const toggleMobileMenu = () => isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true)
+    const toggleMobileMenu = () => {
+        if (width < breakPoints.mobile) {
+            isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true)    
+        }  
+    }  
 
     const style = css`
-        width: 100%;
+        width: 100%; 
         height: 64px;
         display: flex;
         justify-content: center;
@@ -24,7 +28,7 @@ const NavBar = ({ shadow }) => {
         padding: ${spacing.wrapping};
         ${shadow && `box-shadow: rgba(149, 157, 165, 0.15) 0px 8px 20px;`};
         overflow: hidden;
-        
+
         & li {
             font-size: ${font.size.l};
             font-weight: ${font.weight.regular};
@@ -49,7 +53,17 @@ const NavBar = ({ shadow }) => {
             color: ${colors.font.primary};    
         }
     `
-            
+
+    const layer = css`
+        background-color: rgba(0, 0, 0, 0.6);
+        position: absolute;
+        top: 64px;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 100vh;
+    `
+
     const list = css`
         width: ${width > breakPoints.desktop ? spacing.contentWidth : "100%"};
         display: flex;
@@ -67,32 +81,36 @@ const NavBar = ({ shadow }) => {
         background-color: ${colors.background.primary};
         width: 70vw;
         height: 100vh;
-        transition: all .4s; 
-
+        transition: all .4s;
+        ${shadow && `box-shadow: inset rgba(149, 157, 165, 0.15) 0px 8px 20px;`}; 
+        
         & > * + * {
             margin-top: ${spacing.l};
         }    
     `
 
     return (
-        <nav css={style}>
-            {width < breakPoints.mobile && (
-                <div onClick={toggleMobileMenu} css={menuIconContainer}>
-                    {isMenuOpen ? <GrClose /> : <AiOutlineMenu />}
-                </div>
-            )}
-            <ul css={width < breakPoints.mobile ? listMobile : list}>
-                <li onClick={toggleMobileMenu}>
-                    <NavLink to="/">Home</NavLink>
-                </li >
-                <li onClick={toggleMobileMenu}>
-                    <NavLink to="/about">About</NavLink>
-                </li>
-                <li onClick={toggleMobileMenu}>
-                    <NavLink to="/contact">Contact</NavLink>
-                </li>
-            </ul>
-        </nav>
+        <>
+            {isMenuOpen && <div css={layer} onClick={toggleMobileMenu}></div>}
+            <nav css={style}>
+                {width < breakPoints.mobile && (
+                    <div onClick={toggleMobileMenu} css={menuIconContainer}>
+                        {isMenuOpen ? <GrClose /> : <AiOutlineMenu />}
+                    </div>
+                )}
+                <ul css={width < breakPoints.mobile ? listMobile : list}>
+                    <li onClick={toggleMobileMenu}>
+                        <NavLink to="/">Home</NavLink>
+                    </li >
+                    <li onClick={toggleMobileMenu}>
+                        <NavLink to="/about">About</NavLink>
+                    </li>
+                    <li onClick={toggleMobileMenu}>
+                        <NavLink to="/contact">Contact</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </>
     );
 }
 
