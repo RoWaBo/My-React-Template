cd76324bb98ca35566903d041df4c667a660a118
Robert <robertwattboolsen@gmail.com>
2021-10-08 09:47:52 +0200

    Cleaned up NavBar variable names

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,21 +9,28 @@ import { useState } from "react";
 
 const NavBar = ({ shadow }) => {
 
+    // HOOKS
     const { width } = useWindowSize()
     const { colors, font, spacing, breakPoints } = useTheme()
     const [isMenuOpen, setIsMenuOpen] = useState(false)
 
+    // FUNCTIONS
     const toggleMobileMenu = () => isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true)
 
-    const style = css`
+    // VARIABLES
+    const navBarHeight = "64px"
+
+    // === EMOTION STYLING ===
+    const navStyle = css`
         width: 100%;
-        height: 64px;
+        height: ${navBarHeight};
         display: flex;
         justify-content: center;
         align-items: center;
         padding: ${spacing.wrapping};
-        ${shadow && `box-shadow: rgba(149, 157, 165, 0.15) 0px 8px 20px;`};
         overflow: hidden;
+
+        ${shadow && `box-shadow: rgba(149, 157, 165, 0.15) 0px 8px 20px;`};
         
         & li {
             font-size: ${font.size.l};
@@ -39,7 +46,7 @@ const NavBar = ({ shadow }) => {
         }
     `
 
-    const menuIconContainer = css`
+    const menuIconContainerStyle = css`
         position: absolute;
         padding: ${spacing.wrapping};
         left: 0;
@@ -50,7 +57,7 @@ const NavBar = ({ shadow }) => {
         }
     `
             
-    const list = css`
+    const listStyle = css`
         width: ${width > breakPoints.desktop ? spacing.contentWidth : "100%"};
         display: flex;
         justify-content: space-between; 
@@ -59,9 +66,9 @@ const NavBar = ({ shadow }) => {
             margin-left: ${spacing.l};
         }
     `
-    const listMobile = css`
+    const listMobileStyle = css`
         position: absolute;
-        top: 64px;
+        top: ${navBarHeight};
         left: ${isMenuOpen ? "0" : "-70vw"}; 
         padding: ${spacing.wrapping};
         background-color: ${colors.background.primary};
@@ -75,13 +82,13 @@ const NavBar = ({ shadow }) => {
     `
 
     return (
-        <nav css={style}>
+        <nav css={navStyle}>
             {width < breakPoints.mobile && (
-                <div onClick={toggleMobileMenu} css={menuIconContainer}>
+                <div onClick={toggleMobileMenu} css={menuIconContainerStyle}>
                     {isMenuOpen ? <GrClose /> : <AiOutlineMenu />}
                 </div>
             )}
-            <ul css={width < breakPoints.mobile ? listMobile : list}>
+            <ul css={width < breakPoints.mobile ? listMobileStyle : listStyle}>
                 <li onClick={toggleMobileMenu}>
                     <NavLink to="/">Home</NavLink>
                 </li >
